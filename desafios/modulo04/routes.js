8e3c17864c9863b364b266004bb09fc31c0a88e7
caduff97 <kadufernandes.kf@gmail.com>
2020-07-13 10:43:31 -0300

    progress in module 04 and completed its first challenge

---
+++ desafios/modulo04/routes.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const routes = express.Router()
+
+routes.get("/", (req, res) => {
+    return res.redirect("/teachers")
+})
+
+routes.get("/teachers", (req, res) => {
+    return res.render("teachers")
+})
+
+routes.get("/students", (req, res) => {
+    return res.render("students")
+})
+
+module.exports = routes
